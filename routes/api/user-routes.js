25ae339fafb46d6b8ea2749f0fd1915692b35bb4
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-30 17:21:58 -0500

    pseudo descriptions in user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// users' controls
 const {
     getUsers,
     getSingleUser,
@@ -10,10 +11,13 @@ const {
     removeFriend,
 } = requrie('../../controllers/user-controller');
 
+// users api routing for creation *** /api/users
 router.route('/').get(getUsers).post(createUser);
 
+// update and delete users' account *** /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// add and remove users' friend list *** /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;

c5717e3fb755eb41e728a8b2a3a63044408a334e
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-30 17:45:31 -0500

    createUser function is created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,7 +35,9 @@ const userController = {
     },
 
     createUser(req, res) {
-
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
     },
 
     updateUser(req, res) {
